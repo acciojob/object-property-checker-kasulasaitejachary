684064da1c17f3b8ef41d5d10cf0411b74d2a919
Kasula Saitejachary <98974519+kasulasaitejachary@users.noreply.github.com>
2024-06-17 13:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	const flag =-1;
+	sampleObject.map((item)=>{
+		if(key===item){
+			flag=1;
+		}
+	})
+	if(flag===1){
+		return true;
+	}
+	else
+		flase;
 }
 
 // Do not change the code below
